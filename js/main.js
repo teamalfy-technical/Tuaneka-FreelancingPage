09a65c56e68b9fa4531ac164207f2ad0b7a2b523
deanclark1 <ntumydoreen@gmail.com>
2021-04-28 15:30:19 +0000

    added hero section

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(".owl-carousel").on("initialized.owl.carousel", () => {
+    setTimeout(() => {
+      $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
+      $("section").show();
+    }, 200);
+  });
+  
+  const $owlCarousel = $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    responsiveClass:true
+    // nav: true
+  });
+  
+  $owlCarousel.on("changed.owl.carousel", e => {
+    $(".owl-slide-animated").removeClass("is-transitioned");
+  
+    const $currentOwlItem = $(".owl-item").eq(e.item.index);
+    $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
+  
+    const $target = $currentOwlItem.find(".owl-slide-text");
+    // doDotsCalculations($target);
+  });
+  
+  $owlCarousel.on("resize.owl.carousel", () => {
+    setTimeout(() => {
+    //   setOwlDotsPosition();
+    }, 50);
+  });
+
+
+  /*if there isn't content underneath the carousel*/
+  //$owlCarousel.trigger("refresh.owl.carousel");
+  
+//   setOwlDotsPosition();
+  
+//   function setOwlDotsPosition() {
+//     const $target = $(".owl-item.active .owl-slide-text");
+//     doDotsCalculations($target);
+//   }
+  
+//   function doDotsCalculations(el) {
+//     const height = el.height();
+//     const {top, left} = el.position();
+//     const res = height + top + 20;
+  
+//     $(".owl-carousel .owl-dots").css({
+//       top: `${res}px`,
+//       left: `${left}px`
+//     });
+// }
